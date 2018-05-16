f68a60d44ffda51574a5d2ae702796b9550a55ad
shadownic <alexey.plastoon@gmail.com>
2018-05-16 15:08:17 +0300

    list-render

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import React, {
 import './App.css';
 import {USERS} from './assets/users';
 
-const listItems = USERS.map((users) => 
-  <li key = {users.general.firstName.toString()}>
+
+const listItems = USERS.map((users, index) => 
+  <li key = {index} onClick = {(event) => console.log(event.currentTarget.id)} id = {index}>
     <div className = 'image'>
       <img src={users.general.avatar} alt=''></img>
-     </div>  <div className = 'userName'>
+    </div> 
+    <div className = 'userName'>
        <div className='userFirstName'>{users.general.firstName}</div>
        <div className ='userLastName'>{users.general.lastName}</div>
     </div>
-  </li>
+   </li>
 );
 
 class App extends Component {
   render() {
     console.log(USERS);
+    console.log(listItems[2]);
     
     return (  <ul>{listItems}</ul>
     )
